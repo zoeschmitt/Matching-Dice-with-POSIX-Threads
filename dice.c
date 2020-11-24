eea2920a8b91aa5a61951269141d00c378f31e1c
zoeschmitt <zoeschmitt@hotmail.com>
2020-11-24 16:43:28 -0600

    final updates

--- dice.c
+++ dice.c
@@ -7,6 +7,7 @@
 #include <sys/sem.h>
 
 #define NUM_PLAYERS 4
+#define SEM_NAME "rollingsem"
 
 FILE* logFile;
 int seed = 0;
@@ -22,7 +23,6 @@ pthread_mutex_t playerMutex = PTHREAD_MUTEX_INITIALIZER;
 
 void logResults(int player, int rollOne, int rollTwo) {
     char playerL;
-    printf("in log with player %d\n", player);
 
     switch (player) {
     case 0:
@@ -44,8 +44,6 @@ void logResults(int player, int rollOne, int rollTwo) {
     fprintf(logFile, "Player %c: ", playerL);
     fprintf(logFile, "gets %d and %d with a sum of ", rollOne, rollTwo);
     fprintf(logFile, "%d\n", rollOne + rollTwo);
-
-    printf("after log\n");
 }
 
 void finalLog(char player1, char player2) {
@@ -53,76 +51,77 @@ void finalLog(char player1, char player2) {
 }
 
 //checks if anyone has won
-void checkStatus() {
+bool checkStatus() {
     if (sums[0] != 0 && sums[1] != 0 && sums[2] != 0 && sums[3] != 0) {
         if (sums[0] == sums[2]) {
             gameOver = true;
-            pthread_cond_signal(&winner);
+            pthread_cond_broadcast(&winner);
             finalLog('A', 'C');
+            printf("winner A & C\n");
+            return true;
         } else if (sums[1] == sums[3]) {
             gameOver = true;
-            pthread_cond_signal(&winner);
+            pthread_cond_broadcast(&winner);
             finalLog('B', 'D');
+            printf("winner B & D\n");
+            return true;
         }
     }
+    return false;
 }
 
 void* dealer(void* arg) {
    
-    sem_rolling = sem_open("rollingsem", IPC_CREAT, 0660, 1);
-
     upNext = rand() % 4; //gives random number between 0 and 3
-    printf("dealer rolled: %d\n", upNext);
-    pthread_cond_signal(&upNextCond); // signal the players
+    printf("dealer rolled a %d\n", upNext);
+    pthread_cond_broadcast(&upNextCond); // signal the players
 
     pthread_mutex_lock(&dealerMutex); // wait till winner
-
-    while (gameOver == false)
+    while (!gameOver)
         pthread_cond_wait(&winner, &dealerMutex);
-
     pthread_mutex_unlock(&dealerMutex); 
     
-    sem_close(sem_rolling);
     pthread_exit(NULL); 
 }
 
 void roll(int player) {
-    printf("in roll\n");
-    //sem_wait(sem_rolling); // block all threads but one (already have a mutex, but is a failsafe)
     int roll1 = rand() % 7;
-    printf("roll1: %d\n", roll1);
     int roll2 = rand() % 7;
-    printf("roll2: %d\n", roll2);
-    sums[player] = roll1 + roll2; 
-    logResults(player, roll1, roll2); // log the turn
-    //sem_post(sem_rolling); // release
-    printf("finished roll\n");
+    printf("rolled %d and %d\n", roll1, roll2);
+    sums[player] = roll1 + roll2; // update sums array
+    logResults(player, roll1, roll2); // log the turn to file
 }
 
 void* player(void* arg) {
     int *iptr = (int*) arg;
+    bool status = false;
     printf("player thread started: %d\n", *iptr);
     while (!gameOver) {
         pthread_mutex_lock(&playerMutex); 
         while (upNext != *iptr)
             pthread_cond_wait(&upNextCond, &playerMutex);
         pthread_mutex_unlock(&playerMutex); 
-        printf("player: %d rolling\n", *iptr);
-        roll(*iptr);
-        
-        checkStatus();
-        upNext >= 3 ? upNext = 0 : upNext++;
-        pthread_cond_signal(&upNextCond);
-        printf("updated\n");
+        status =  checkStatus(); // make sure someone else hasn't won already
+        if (!status) {
+            printf("player %d rolling\n", *iptr);
+            sem_wait(sem_rolling);  // make sure only one person is rolling at a time
+            roll(*iptr);
+            sem_post(sem_rolling);
+            upNext >= 3 ? upNext = 0 : upNext++; // next turn
+            pthread_cond_broadcast(&upNextCond); // let other players know its next turn
+        } else 
+            pthread_exit(NULL); 
     }
     pthread_exit(NULL); 
 }
 
 int main (int argc, char *argv[]) {
+    sem_unlink(SEM_NAME); // just incase leftover sem from other run
     int players[] = {0, 1, 2, 3};
     logFile = fopen("logfile.txt", "w");
     seed = atoi(argv[1]);
     srand(seed);
+    sem_rolling = sem_open(SEM_NAME, O_RDWR, 0777, 1); // init semaphore
     pthread_t playerA;
     pthread_t playerB;
     pthread_t playerC;
@@ -135,6 +134,7 @@ int main (int argc, char *argv[]) {
     pthread_create(&playerD, NULL, player, &players[3]);
 
     pthread_join(dealerThread, NULL);
+    sem_close(sem_rolling);
     fclose(logFile);
-    pthread_exit(NULL); 
+    exit(EXIT_SUCCESS);
 }
